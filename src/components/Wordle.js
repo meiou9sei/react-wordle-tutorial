7eec078985f9318f60f0f348f816d409ab7dab29
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-08-17 10:49:30 -0400

    create add new guesses

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -3,15 +3,22 @@ import { useEffect } from 'react';
 import useWordle from '../hooks/useWordle';
 
 export default function Wordle({ solution }) {
-    const { currentGuess, handleKeyup } = useWordle(solution);
+    const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(solution);
 
     useEffect(() => {
-        window.addEventListener('keyup', handleKeyup)
+        window.addEventListener('keyup', handleKeyup);
 
-        return () => window.removeEventListener('keyup', handleKeyup)
+        return () => window.removeEventListener('keyup', handleKeyup);
     }, [handleKeyup])
+
+    useEffect(() => {
+        console.log(guesses, turn, isCorrect);
+    }, [guesses, turn, isCorrect])
   
     return (
-        <div>current guess - {currentGuess}</div>
+        <div>
+            <div>solution - {solution}</div>
+            <div>current guess - {currentGuess}</div>
+        </div>
     )
 }
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,23 +1,58 @@
 import { useState } from "react"
 
-const useWordle = () => {
+const useWordle = (solution) => {
     const [turn, setTurn] = useState(0);
     const [currentGuess, setCurrentGuess] = useState('');
-    const [guesses, setGuesses] = useState([]); //each guess is an array
-    const [history, setHistory] = useState(['ninja', 'guess']); //each guess is a string
+    const [guesses, setGuesses] = useState([...Array(6)]); //each guess is an array
+    const [history, setHistory] = useState([]); //each guess is a string
     const [isCorrect, setIsCorrect] = useState(false);
 
     //format a guess into an array of letter objects
     //e.g. [{key: 'a', color: 'yellow'}]
     const formatGuess = () => {
-        console.log('formatting the guess - ', currentGuess)
+        let solutionArray = [...solution];
+        let formattedGuess = [...currentGuess].map((l) => {
+            return {key: l, color: 'grey'};
+        })
+
+        //find any green letters
+        formattedGuess.forEach((l, i) => {
+            if (solutionArray[i] === l.key) {
+                formattedGuess[i].color = 'green';
+                solutionArray[i] = null;
+            }
+        })
+
+        //find any yellow colors
+        formattedGuess.forEach((l, i) => {
+            if (solutionArray.includes(l.key) && l.color !== 'green') {
+                formattedGuess[i].color = 'yellow';
+                solutionArray[solutionArray.indexOf(l.key)] = null;
+            }
+        })
+
+        return formattedGuess;
     }
 
     //add a new guess to the guesses state
     //update the isCorrect state if the guess is correct
     //add one to the turn state
-    const addNewGuess = () => {
-
+    const addNewGuess = (formattedGuess) => {
+        if (currentGuess === solution) {
+            setIsCorrect(true);
+        }
+        setGuesses((prevGuesses) => {
+            let newGuesses = [...prevGuesses];
+            newGuesses[turn] = formattedGuess;
+            return newGuesses;
+        })
+        setHistory((prevHistory) => {
+            return [...prevHistory, currentGuess];
+        })
+        setTurn((prevTurn) => {
+            return prevTurn + 1;
+        })
+        setCurrentGuess('');
     }
 
     //handle keyup event & track current guess
@@ -39,7 +74,8 @@ const useWordle = () => {
                 console.log('word must be 5 chars long');
                 return;
             }
-            formatGuess();
+            const formatted = formatGuess();
+            addNewGuess(formatted);
         }
         
         if (key === 'Backspace') {
