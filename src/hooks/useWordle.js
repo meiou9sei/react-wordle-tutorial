1b900f2cfd7c79810e5c4ad5d5ddfda2edcf81ca
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-08-17 10:03:50 -0400

    create submit guess

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,13 +4,13 @@ const useWordle = () => {
     const [turn, setTurn] = useState(0);
     const [currentGuess, setCurrentGuess] = useState('');
     const [guesses, setGuesses] = useState([]); //each guess is an array
-    const [history, setHistory] = useState([]); //each guess is a string
+    const [history, setHistory] = useState(['ninja', 'guess']); //each guess is a string
     const [isCorrect, setIsCorrect] = useState(false);
 
     //format a guess into an array of letter objects
     //e.g. [{key: 'a', color: 'yellow'}]
     const formatGuess = () => {
-
+        console.log('formatting the guess - ', currentGuess)
     }
 
     //add a new guess to the guesses state
@@ -23,6 +23,24 @@ const useWordle = () => {
     //handle keyup event & track current guess
     //if user presses enter, add the new guess
     const handleKeyup = ({ key }) => {
+        if (key === 'Enter') {
+            //only add guess if turn is less than 5
+            if (turn > 5) {
+                console.log('you used all your guesses');
+                return;
+            }
+            //do not allow duplicate words
+            if (history.includes(currentGuess)) {
+                console.log('you already tried that word');
+                return;
+            }
+            //check word is 5 chars long
+            if (currentGuess.length !== 5) {
+                console.log('word must be 5 chars long');
+                return;
+            }
+            formatGuess();
+        }
         
         if (key === 'Backspace') {
             setCurrentGuess((prev) => {
