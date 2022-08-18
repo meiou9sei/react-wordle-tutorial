1ff1672490b85cf5dcaf6954eab73241840330e2
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-08-18 10:30:27 -0400

    add game ending conditions

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -10,12 +10,18 @@ export default function Wordle({ solution }) {
     useEffect(() => {
         window.addEventListener('keyup', handleKeyup);
 
-        return () => window.removeEventListener('keyup', handleKeyup);
-    }, [handleKeyup])
+        if (isCorrect) {
+            console.log('congrats');
+            window.removeEventListener('keyup', handleKeyup);
+        }
+        
+        if (turn > 5) {
+            console.log('game over');
+            window.removeEventListener('keyup', handleKeyup);
+        }
 
-    useEffect(() => {
-        console.log(guesses, turn, isCorrect);
-    }, [guesses, turn, isCorrect])
+        return () => window.removeEventListener('keyup', handleKeyup);
+    }, [handleKeyup, isCorrect, turn])
   
     return (
         <div>
